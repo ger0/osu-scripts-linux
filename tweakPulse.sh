048c6ee74247b7764abe18881ebace54d78e0d3e
gero <mrgero155@gmail.com>
2018-03-31 20:21:20 +0200

    Update tweakPulse.sh

--- tweakPulse.sh
+++ tweakPulse.sh
@@ -12,6 +12,6 @@ cp /etc/pulse/daemon.conf ~/.config/pulse/
 sed -i -e "s/load-module-udev-detect .$/load-module-ude-detect tsched=0/g" default.pa
 sed -i -e "s/; high/high/g" daemon.conf
 sed -i -e "s/; realtime/realtime/g" daemon.conf
-sed -i -e "s/default-fragments =.$/default-fragments = 1/g" daemon.conf
-sed -i -e "s/default-fragment-size-msec =.$/default-fragment-size-msec = 1/g" daemon.conf
+sed -i -e "s/; default-fragments =.$/default-fragments = 1/g" daemon.conf
+sed -i -e "s/; default-fragment-size-msec =.$/default-fragment-size-msec = 1/g" daemon.conf
 echo Done!
